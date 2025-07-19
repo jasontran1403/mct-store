78e58ea8b07fdbd0ce71f8df1ecd508b32baba93
jasontran1403 <hainguyentran14@gmail.com>
2025-07-19 13:31:34 +0700

    first commit

---
+++ src/components/footer/FooterCopyright.js
@@ -0,0 +1,25 @@
+import PropTypes from "prop-types";
+import React from "react";
+
+const FooterCopyright = ({ footerLogo, spaceBottomClass, colorClass }) => {
+  return (
+    <div
+      className={`copyright ${spaceBottomClass ? spaceBottomClass : ""} ${
+        colorClass ? colorClass : ""
+      }`}
+    >
+      <p>
+        &copy; {new Date().getFullYear()}{" "}
+        Mapchain.<br /> All Rights Reserved
+      </p>
+    </div>
+  );
+};
+
+FooterCopyright.propTypes = {
+  footerLogo: PropTypes.string,
+  spaceBottomClass: PropTypes.string,
+  colorClass: PropTypes.string
+};
+
+export default FooterCopyright;
